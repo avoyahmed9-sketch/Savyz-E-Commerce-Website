3d862263ab9c0eb8aeccc4888194f7b738b63a0e
Rafin Raihan <avoyahmed9@gmail.com>
2026-01-03 19:12:36 +0600

    New register page is added.

---
+++ register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="register.php" method="post">
+       <input type="text" name="name" placeholder="Enter your name here!" required>
+       <input type="email" name="email" placeholder="Enter your email here!" required>
+       <input type="password" name="password" placeholder="Enter your password here!" required>
+       <input type="text" name="phone" placeholder="Enter your phone number!" required>
+       <input type="text" name="phone" placeholder="Enter your phone number!" required>
+       <textarea name="address" placeholder="Enter your address here"></textarea>
+       <input type="submit" name="submit" value="Sign up">
+
+
+
+
+    </form>
+</body>
+</html>
