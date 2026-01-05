f72fda85d2a10d29849502f5652811e6ae405dde
Rafin Raihan <avoyahmed9@gmail.com>
2026-01-05 21:58:33 +0600

    Basic HTML on Login page

--- login.php
+++ login.php
@@ -1,6 +1,23 @@
 <?php
 
 
-
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="login.php" method="post">
+        <input type="email" name="email" placeholder="Enter you email:" required>
+        <input type="password" name="password" placeholder="Enter your password:" required>
+        <input type="submit" name="submit" value="login">
+        <p>Don't Register Yet!
+            <a href="register.php">Sign up</a>
+        </p>
+    </form>
+    
+</body>
+</html>
