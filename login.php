e14e829b62b453792629e1d0f645d73d761b77a5
Rafin Raihan <avoyahmed9@gmail.com>
2026-01-05 22:06:51 +0600

    Login page has been  included the style tag

--- login.php
+++ login.php
@@ -7,9 +7,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+    <style>
+
+
+
+
+
+
+
+    </style>
 </head>
 <body>
+    <div class="login">
     <form action="login.php" method="post">
         <input type="email" name="email" placeholder="Enter you email:" required>
         <input type="password" name="password" placeholder="Enter your password:" required>
@@ -18,6 +28,7 @@
             <a href="register.php">Sign up</a>
         </p>
     </form>
+    </div>
     
 </body>
 </html>
