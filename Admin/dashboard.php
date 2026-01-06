c0265180239f8f1d206e4853149ebb102bc8e514
Rafin Raihan <avoyahmed9@gmail.com>
2026-01-06 13:18:47 +0600

    Dashboard has session now

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -1,3 +1,16 @@
  <?php
+ session_start();
+ if(isset($_SESSION['user_id'])){
+   if($_SESSION['user_role'] == "admin"){
+
+   }
+   else{
+    echo "Go for user dashboard";
+   }
+ }
+ else{
+    header("Location: ../index.php");
+ }
 
  ?>
+
